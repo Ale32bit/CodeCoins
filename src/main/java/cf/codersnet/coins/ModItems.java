fc4a4f00cd492c9c1e9b993a112fc05c0ef97a3f
Ale32bit <ale.proto00@gmail.com>
2017-06-23 15:25:12 +0200

    Fixed spaces

--- src/main/java/cf/codersnet/coins/ModItems.java
+++ src/main/java/cf/codersnet/coins/ModItems.java
@@ -6,20 +6,20 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class ModItems {
 	
 	public static ItemBase oneCoin;
-  public static ItemBase fiveCoin; //new
+	public static ItemBase fiveCoin; //new
 	public static ItemBase tenCoin;
-  public static ItemBase twentyCoin; //new
-  public static ItemBase fiftyCoin; //new
+	public static ItemBase twentyCoin; //new
+	public static ItemBase fiftyCoin; //new
 	public static ItemBase hundredCoin;
 	public static ItemBase fiveHundredCoin;
 	public static ItemBase thousandCoin;
 
 	public static void init() {
 		oneCoin = register(new ItemBase("oneCoin"));
-    fiveCoin = register(new ItemBase("fiveCoin"));
+		fiveCoin = register(new ItemBase("fiveCoin"));
 		tenCoin = register(new ItemBase("tenCoin"));
-    twentyCoin = register(new ItemBase("twentyCoin"));
-    fiftyCoin = register(new ItemBase("fiftyCoin"));
+		twentyCoin = register(new ItemBase("twentyCoin"));
+		fiftyCoin = register(new ItemBase("fiftyCoin"));
 		hundredCoin = register(new ItemBase("hundredCoin"));
 		fiveHundredCoin = register(new ItemBase("fiveHundredCoin"));
 		thousandCoin = register(new ItemBase("thousandCoin"));
